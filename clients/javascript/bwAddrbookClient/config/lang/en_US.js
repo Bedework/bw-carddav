1a29519e2f22e260cbaca595676e097f19aa31e0
Arlen Johnson <johnsa@rpi.edu>
2010-10-13 17:58:19 +0000

    javascript client: catch various server errors and provide useful feedback to the user

--- clients/javascript/bwAddrbookClient/config/lang/en_US.js
+++ clients/javascript/bwAddrbookClient/config/lang/en_US.js
@@ -49,7 +49,7 @@ var bwAbDispUnimplementedTitle = "Unimplemented";
 var bwAbDispUnimplemented = "This feature is unimplemented.";
 var bwAbDispErrorTitle = "Error";
 var bwAbDispError404 = "<h3>You cannot access the current address book.</h3><p>It is likely that you are still authenticated as another user.  You must invalidate your HTTP authentication or close your browser and try again.</p>"; 
-var bwAbDispError500 = '<h3>Internal Server Error</h3><p>We\'re still in development. Please try <a href="javascript:window.location.reload();">reloading</a> your browser.</p>';
+var bwAbDispError500 = '<h3>Internal Server Error</h3><p>We\'re still in development.<p><p>If you do not see an address book listing and expect one, please try <a href="javascript:window.location.reload();">reloading</a> your browser.</p><p>(You may otherwise ignore and close this message.)</p>';
 var bwAbDispErrorAccessDenied = "Access Denied";
 
 // add/update messages
