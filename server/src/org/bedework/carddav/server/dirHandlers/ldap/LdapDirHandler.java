9e394e93894ba3e06e099f9f80608de3e9d9b0ea
Mike Douglass <douglm@rpi.edu>
2012-06-06 19:19:50 +0000

    Trivial comment change

--- server/src/org/bedework/carddav/server/dirHandlers/ldap/LdapDirHandler.java
+++ server/src/org/bedework/carddav/server/dirHandlers/ldap/LdapDirHandler.java
@@ -6,9 +6,9 @@
     Version 2.0 (the "License"); you may not use this file
     except in compliance with the License. You may obtain a
     copy of the License at:
-        
+
     http://www.apache.org/licenses/LICENSE-2.0
-        
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the License is distributed on
     an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -917,7 +917,7 @@ public abstract class LdapDirHandler extends AbstractDirHandler {
   }
 
   /* Do the search for a single object in the directory
-\   */
+   */
   protected Attributes getObject(final String path,
                              final boolean isCollection) throws WebdavException {
     try {
