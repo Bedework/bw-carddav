40eebe1c3facf09413f063580c075ebf13fcfda2
mike <mikeadouglass@gmail.com>
2018-12-14 00:52:47 -0500

    Back off because release failed

--- bw-carddav-server/src/main/java/org/bedework/carddav/server/CarddavServlet.java
+++ bw-carddav-server/src/main/java/org/bedework/carddav/server/CarddavServlet.java
@@ -20,6 +20,7 @@ package org.bedework.carddav.server;
 
 import org.bedework.carddav.server.jmx.CardDav;
 import org.bedework.util.jmx.ConfBase;
+import org.bedework.util.logging.BwLogger;
 import org.bedework.webdav.servlet.common.MethodBase.MethodInfo;
 import org.bedework.webdav.servlet.common.WebdavServlet;
 import org.bedework.webdav.servlet.shared.WebdavException;
@@ -121,5 +122,20 @@ public class CarddavServlet extends WebdavServlet
   @Override
   public void contextDestroyed(final ServletContextEvent sce) {
     conf.stop();
+  }
+
+  /* ====================================================================
+   *                   Logged methods
+   * ==================================================================== */
+
+  private BwLogger logger = new BwLogger();
+
+  @Override
+  public BwLogger getLogger() {
+    if ((logger.getLoggedClass() == null) && (logger.getLoggedName() == null)) {
+      logger.setLoggedClass(getClass());
+    }
+
+    return logger;
   }
 }
